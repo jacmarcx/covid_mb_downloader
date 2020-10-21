178020c9593268bed862ae323d8f4866023c4585
Jacques Marcoux <29962430+jacmarcx@users.noreply.github.com>
2020-10-21 00:39:49 -0500

    remove while loop

--- covid_downloader.py
+++ covid_downloader.py
@@ -38,27 +38,25 @@ def dl_file(url, path, file, ext='.json'):
             repo.create_file(path+stamp+file+ext, commit, data)
 
 
-while True:
+# MB - Cases by demographics and RHA
+dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_cases_by_demographics_rha_all/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=Age_Group%2CGender&orderByFields=Age_Group%20desc',
+        'json/demographics-rha/',
+        'demographics-rha')
 
-    # MB - Cases by demographics and RHA
-    dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_cases_by_demographics_rha_all/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=Age_Group%2CGender&orderByFields=Age_Group%20desc',
-            'json/demographics-rha/',
-            'demographics-rha')
+# MB - Cases by status and RHA
+dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_cases_by_status_daily_rha/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=Date%2CRHA',
+        'json/case-summary-rha/',
+        'case-summary-rha')
 
-    # MB - Cases by status and RHA
-    dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_cases_by_status_daily_rha/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=Date%2CRHA',
-            'json/case-summary-rha/',
-            'case-summary-rha')
+# MB - Manitoba five-day test positivity rate
+dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_5_day_positivity_rate/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc',
+        'json/test-positivity-rate/',
+        'test-positivity-rate')
+        
+# MB - Manitoba district level data
+dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/ArcGIS/rest/services/mb_covid_cases_summary_stats_geography/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true',
+        'json/details-district/',
+        'details-district')
+        
 
-    # MB - Manitoba five-day test positivity rate
-    dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/arcgis/rest/services/mb_covid_5_day_positivity_rate/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc',
-            'json/test-positivity-rate/',
-            'test-positivity-rate')
-            
-    # MB - Manitoba district level data
-    dl_file('https://services.arcgis.com/mMUesHYPkXjaFGfS/ArcGIS/rest/services/mb_covid_cases_summary_stats_geography/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true',
-            'json/details-district/',
-            'details-district')
-            
-    time.sleep(86400) #24 hours
     
